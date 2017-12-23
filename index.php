00d4f27ce00a06caf25d632932314c8075c95716
denish <denish.freelancer@gmail.com>
2017-12-23 10:44:39 +0545

    initial commit added

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 	<title>Title</title>
 </head>
 <body>
+<h1>Description</h1>
 <p>
-	Lorem islam
-</p>
+clear clears your screen if this is possible. It looks in the environment for the terminal type and then in the terminfo database to figure out how to clear the screen.
 
+clear ignores any command-line parameters that may be present.
+</p>
 </body>
 </html>
