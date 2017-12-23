c733e53c07bad38d5530f9a395eab1deb71bcc8b
denish <denish.freelancer@gmail.com>
2017-12-23 10:47:09 +0545

    new lines of code added

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
 	<title>Title</title>
 </head>
 <body>
-<h1>Description</h1>
+<h1>The great ghost</h1>
 <p>
 clear clears your screen if this is possible. It looks in the environment for the terminal type and then in the terminfo database to figure out how to clear the screen.
-
+<br>
 clear ignores any command-line parameters that may be present.
 </p>
+
+<p>This describes ncurses version 5.7 (patch 20090207).</p>
 </body>
 </html>
